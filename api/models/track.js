c81b409cb8a3e89b4080a1288d01598dd799078e
Acacia Mack <acacialmack@gmail.com>
2016-04-02 12:27:45 -0400

    added user schema

---
+++ api/models/track.js
@@ -0,0 +1,11 @@
+// var mongoose = require('mongoose')
+//
+// var userSchema = mongoose.Schema({
+//   name: String,
+//   email: String,
+//   picture: String,
+//   soundcloudId: String,
+//   tracks: []
+// });
+//
+// module.exports = mongoose.model('User', userSchema);
---
+++ api/models/user.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose')
+
+var userSchema = mongoose.Schema({
+  name: String,
+  email: String,
+  picture: String,
+  soundcloudId: String
+});
+
+module.exports = mongoose.model('User', userSchema);
