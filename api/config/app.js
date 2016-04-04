3f3a51442588df2dc03074b201e776d29cd14c8a
Acacia Mack <acacialmack@gmail.com>
2016-04-04 05:07:00 -0400

    login in with soundcloud working

--- api/config/app.js
+++ api/config/app.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  databaseUrl: 'mongodb://localhost/satellizer',
+  databaseUrl: 'mongodb://localhost/studioVibes',
   secret: 'notVerySecret',
   appUrl: 'http://localhost:8000'
 }
