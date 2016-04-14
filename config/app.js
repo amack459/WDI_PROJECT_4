42a5b42ea1c82af7cf209f1330312a9b85c816f2
Acacia Mack <acacialmack@gmail.com>
2016-04-14 09:35:53 -0400

    Updated mongo uri

--- config/app.js
+++ config/app.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 8000,
-  databaseUrl: process.env.MONGOLAB_URI || 'mongodb://localhost/studioVibes',
+  databaseUrl: process.env.MONGODB_URI || 'mongodb://localhost/studioVibes',
   secret: 'notVerySecret'
 }
